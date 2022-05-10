1b47fdb9ebc45cfc396e044af23c77831f9ec9d1
lone <lone@yousails.com>
2022-05-10 12:07:21 +0800

    更改品牌名称和样式细节

--- frontend/src/components/shop_item.rs
+++ frontend/src/components/shop_item.rs
@@ -12,19 +12,26 @@ pub struct ShopItemProps {
 pub fn shop_item(ShopItemProps { shop, show_more } : &ShopItemProps) -> Html {
 	html! {
 		<div class="shop-card">
-            <div class="shop-card-images">
-                <img src={shop.cover_image.clone()} alt={shop.name.clone()} />
-            </div>
+			if *show_more {
+	            <div class="shop-card-images">
+	                <img src={shop.cover_image.clone()} alt={shop.name.clone()} />
+	            </div>
+        	}
             <div class="shop-card-content">
                 <p class="shop-card-name">
-                	<span>{format!("{}", shop.name)}</span>
-                	<span class="shop-mark-button">{"📷 拍照打卡 🌟"}</span>
+                	<div class="shop-card-name-wrap">
+                		<span>{format!("{}", shop.name)}</span>
+                		<span class="shop-card-state">{"正常营业"}</span>
+                	</div>
+                	<div class="shop-mark-button">
+                		<img src="https://s1.ax1x.com/2022/05/10/OYX2Zt.jpg" />
+                	</div>
                 </p>
+                <p>{format!("🏷️ {}", shop.tags)}</p>
                 <p>{format!("地址: {}", shop.address)}</p>
                 if *show_more {
 	                <p>{format!("营业时间: {}", shop.open_time)}</p>
 	                <p>{format!("联系方式: {}", shop.telephone)}</p>
-	                <p>{format!("标签: {}", shop.tags)}</p>
 	                <p class="shop-card-desc">{format!("简介: {}", shop.desc)}</p>
             	}
             </div>
